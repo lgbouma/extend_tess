c4a15a3c2fb3087e0c8a7ef3faefe9231a5d40e2
lgbouma <lgbouma@mit.edu>
2018-12-03 10:07:17 -0500

    add bulk_copy, which renames for...
    renames for `/results/planet_yield_plots/all_ideas_one_directory`

---
+++ src/bulk_copy.py
@@ -0,0 +1,29 @@
+import os
+from shutil import copyfile
+from glob import glob
+
+def main():
+
+    savdir = '../results/planet_yield_plots/all_ideas_one_directory/'
+    names = ['idea_1_SNE','idea_2_SNSNS','idea_3_SNNSN','idea_4_EC3PO']
+    outdirs = [os.path.join('../results/planet_yield_plots/',dirname)
+               for dirname in names]
+
+    for outdir in outdirs:
+        fpaths = glob(os.path.join(outdir,'*png'))
+
+        if len(fpaths)==0:
+            print('got no files in {}'.format(outdir))
+
+        else:
+            for fpath in fpaths:
+                newname = fpath.split('/')[-1].rstrip('.png')
+                newname += fpath.split('/')[-2]
+                newname += '.png'
+                outpath = os.path.join(savdir,newname)
+                copyfile(fpath, outpath)
+                print('{} --> {}'.format(fpath, outpath))
+
+if __name__=="__main__":
+
+    main()
