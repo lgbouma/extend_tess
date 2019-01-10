d11ef3a7737bdff4894cb21f35b9cc5d9b0c3c9a
lgbouma <lgbouma@mit.edu>
2019-01-10 18:24:44 -0500

    update planet yield plots for proposal

--- src/planet_yield_plots.py
+++ src/planet_yield_plots.py
@@ -138,28 +138,53 @@ def radius_period_diagram_multitransiting(df, xlim=(0.3,500), ylim=(0.3,30),
     print('made {}'.format(savpath))
 
 
-def radius_insolation_diagram(df, xlim=None, ylim=None, outdir=None):
+def radius_insolation_diagram(df, xlim=None, ylim=None, outdir=None,
+                              forproposal=False):
 
-    fig,ax = plt.subplots(figsize=(6,4))
+    fig,ax = plt.subplots(figsize=(4,3))
 
     in_prime = nparr(df['detected_primary'])
     in_ext = nparr(df['detected']) & ~in_prime
 
+    in_hz = nparr( (df['insol']>0.5) & (df['insol']<2.0) )
+
+    if forproposal:
+        s = 20
+    else:
+        s = 10
     ax.scatter(df.loc[in_prime, 'insol'],
                df.loc[in_prime, 'planetRadius'],
-               label='detected after primary', lw=0, alpha=0.8, s=10)
+               label='Prime: {} in HZ'.format(int(len(df[in_prime & in_hz]))),
+               lw=0, alpha=0.8, s=s)
 
     ax.scatter(df.loc[in_ext, 'insol'],
                df.loc[in_ext, 'planetRadius'],
-               label='detected after extended', lw=0, alpha=0.8, s=10)
+               label='Extended: {} in HZ'.format(int(len(df[df['detected'] & in_hz]))),
+               lw=0, alpha=0.8, s=s)
 
     ax.set_ylabel('Radius [$R_\oplus$]')
     ax.set_xlabel('Insolation [$S_\oplus$]')
 
-    ax.set_xscale('log')
-    ax.set_yscale('log')
+    if not forproposal:
+        ax.set_xscale('log')
+        ax.set_yscale('log')
 
-    ax.legend(loc='best', fontsize='xx-small')
+    if forproposal:
+        fs = 8
+    else:
+        fs = 'small'
+    leg = ax.legend(loc='best', fontsize=fs, handletextpad=0.1)
+    leg.get_frame().set_alpha(1)
+
+    if forproposal:
+        hz_x = np.linspace(0.5,2,100)
+        hz_ylower = np.zeros_like(hz_x)
+        hz_yupper = np.ones_like(hz_x)*40
+
+        ax.vlines(0.5, 0, 40, color='green', alpha=0.9, zorder=-1)
+        ax.vlines(2, 0, 40, color='green', alpha=0.9, zorder=-1)
+        ax.fill_between(hz_x, hz_ylower, hz_yupper, color='green', alpha=0.3,
+                        zorder=-2)
 
     ax.get_yaxis().set_tick_params(which='both', direction='in')
     ax.get_xaxis().set_tick_params(which='both', direction='in')
@@ -170,11 +195,34 @@ def radius_insolation_diagram(df, xlim=None, ylim=None, outdir=None):
         ax.set_ylim(ylim)
 
     titlestr = os.path.basename(outdir).replace('_',' ')
-    ax.set_title(titlestr, fontsize='x-small')
+    if not forproposal:
+        ax.set_title(titlestr, fontsize='x-small')
 
-    savpath = os.path.join(outdir,'radius_insolation_diagram.png')
+    outstr='' if not forproposal else '_forproposal'
+    savpath = os.path.join(outdir,'radius_insolation_diagram{}.png'.format(outstr))
     fig.savefig(savpath, bbox_inches='tight', dpi=400)
     print('made {}'.format(savpath))
+
+
+def autolabelforproposal(rects, ax, counts):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect, count in zip(rects, counts):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 0.99*height,
+                '{:d}'.format(count),
+                ha='center', va='top')
+
+def autolabelforproposal2(rects, ax, counts):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect, count in zip(rects, counts):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 0.95*height,
+                '{:d}'.format(count),
+                ha='center', va='top')
 
 
 def autolabel(rects, ax):
@@ -200,7 +248,8 @@ def autolabel2(rects, ax):
 
 def number_of_detections_vs_plradius_barchart(df, txt=None, ylim=None,
                                               justmultis=False, outstr='',
-                                              yscale=None, outdir=None):
+                                              yscale=None, outdir=None,
+                                              forproposal=False):
 
     in_pri = nparr(df['detected_primary'])
     after_ext = nparr(df['detected'])
@@ -229,34 +278,50 @@ def number_of_detections_vs_plradius_barchart(df, txt=None, ylim=None,
     counts_pri = np.histogram(df.loc[in_pri, 'planetRadius'],
                               bins=[0,1.25,2,4,25])
 
+    yval_ext = counts_ext[0]
+    yval_pri = counts_pri[0]
+
+    if forproposal:
+        yval_ext = yval_ext/yval_pri
+        yval_pri = yval_pri/yval_pri
+
     tl = ['$<1.25$', '$1.25-2.0$', '$2.0-4.0$', '$>4.0$']
 
     plt.close("all")
-    fig, ax = plt.subplots(1,1,figsize=[6,6])
+    fig, ax = plt.subplots(1,1,figsize=[4,4])
 
     if outstr not in ['oneortwotraonly_']:
-        h_pri = ax.bar(np.arange(4), counts_pri[0], tick_label=tl,
-                       label='after primary', zorder=2, color='#1f77b4')
-    h_ext = ax.bar(np.arange(4), counts_ext[0], tick_label=tl,
-                   label='after extended', zorder=1, color='#ff7f0e')
+        h_pri = ax.bar(np.arange(4), yval_pri, tick_label=tl,
+                       label='Prime', zorder=2, color='#1f77b4')
+    h_ext = ax.bar(np.arange(4), yval_ext, tick_label=tl,
+                   label='Extended', zorder=1, color='#ff7f0e')
 
-    ax.set_xlabel('Planet radii ($ R_\oplus$)')
-    ax.set_ylabel('Number of planets')
-
-    if yscale:
-        ax.set_yscale(yscale)
+    ax.set_xlabel('Planet radius ($ R_\oplus$)')
+    if not forproposal:
+        ax.set_ylabel('Number of planets')
     else:
-        ax.set_yscale('log')
+        ax.set_ylabel('Planets detected (normalized by Prime)')
 
-    if txt:
-        ax.text(0.05, 0.8, txt, ha='left', va='center', fontsize='x-small',
-                transform=ax.transAxes)
+    if not forproposal:
+        if yscale:
+            ax.set_yscale(yscale)
+        else:
+            ax.set_yscale('log')
 
-    autolabel(h_ext, ax)
-    if outstr not in ['oneortwotraonly_']:
-        autolabel2(h_pri, ax)
+    if not forproposal:
+        if txt:
+            ax.text(0.05, 0.8, txt, ha='left', va='center', fontsize='x-small',
+                    transform=ax.transAxes)
 
-    ax.legend()
+    if forproposal:
+        autolabelforproposal(h_ext, ax, counts_ext[0])
+        autolabelforproposal2(h_pri, ax, counts_pri[0])
+    else:
+        autolabel(h_ext, ax)
+        if outstr not in ['oneortwotraonly_']:
+            autolabel2(h_pri, ax)
+
+    ax.legend(fontsize='small')
 
     if ylim:
         ax.set_ylim(ylim)
@@ -264,18 +329,22 @@ def number_of_detections_vs_plradius_barchart(df, txt=None, ylim=None,
     ax.get_yaxis().set_tick_params(which='both', direction='in')
     ax.get_xaxis().set_tick_params(which='both', direction='in')
 
-    titlestr = os.path.basename(outdir).replace('_',' ')
-    ax.set_title(titlestr, fontsize='x-small')
+    if not forproposal:
+        titlestr = os.path.basename(outdir).replace('_',' ')
+        ax.set_title(titlestr, fontsize='x-small')
 
+    eoutstr='' if not forproposal else '_forproposal'
     savpath = os.path.join(
-        outdir,outstr+'number_of_detections_vs_plradius_barchart.png')
+        outdir,outstr+'number_of_detections_vs_plradius_barchart{}.png'.
+        format(eoutstr))
     fig.savefig(savpath, bbox_inches='tight', dpi=400)
     print('made {}'.format(savpath))
 
 
 def number_of_detections_vs_period_barchart(df, txt=None, ylim=None,
                                             justmultis=False, outstr='',
-                                            yscale=None, outdir=None):
+                                            yscale=None, outdir=None,
+                                            forproposal=False):
 
     in_pri = nparr(df['detected_primary'])
     after_ext = nparr(df['detected'])
@@ -304,34 +373,50 @@ def number_of_detections_vs_period_barchart(df, txt=None, ylim=None,
     counts_pri = np.histogram(df.loc[in_pri, 'planetPeriod'],
                               bins=[0,20,50,1000])
 
+    yval_ext = counts_ext[0]
+    yval_pri = counts_pri[0]
+
+    if forproposal:
+        yval_ext = yval_ext/yval_pri
+        yval_pri = yval_pri/yval_pri
+
     tl = ['$<20$', '$20-50$', '$>50$']
 
     plt.close("all")
-    fig, ax = plt.subplots(1,1,figsize=[6,6])
+    fig, ax = plt.subplots(1,1,figsize=[4,4])
 
     if outstr not in ['oneortwotraonly_']:
-        h_pri = ax.bar(np.arange(3), counts_pri[0], tick_label=tl,
-                       label='after primary', zorder=2, color='#1f77b4')
-    h_ext = ax.bar(np.arange(3), counts_ext[0], tick_label=tl,
-                   label='after extended', zorder=1, color='#ff7f0e')
+        h_pri = ax.bar(np.arange(3), yval_pri, tick_label=tl,
+                       label='Prime', zorder=2, color='#1f77b4')
+    h_ext = ax.bar(np.arange(3), yval_ext, tick_label=tl,
+                   label='Extended', zorder=1, color='#ff7f0e')
 
     ax.set_xlabel('Orbital period (days)')
-    ax.set_ylabel('Number of planets')
-
-    if yscale:
-        ax.set_yscale(yscale)
+    if not forproposal:
+        ax.set_ylabel('Number of planets')
     else:
-        ax.set_yscale('log')
+        ax.set_ylabel('Planets detected (normalized by Prime)')
 
-    if txt:
-        ax.text(0.05, 0.8, txt, ha='left', va='center', fontsize='x-small',
-                transform=ax.transAxes)
+    if not forproposal:
+        if yscale:
+            ax.set_yscale(yscale)
+        else:
+            ax.set_yscale('log')
 
-    autolabel(h_ext, ax)
-    if outstr not in ['oneortwotraonly_']:
-        autolabel2(h_pri, ax)
+    if not forproposal:
+        if txt:
+            ax.text(0.05, 0.8, txt, ha='left', va='center', fontsize='x-small',
+                    transform=ax.transAxes)
 
-    ax.legend()
+    if forproposal:
+        autolabelforproposal(h_ext, ax, counts_ext[0])
+        autolabelforproposal2(h_pri, ax, counts_pri[0])
+    else:
+        autolabel(h_ext, ax)
+        if outstr not in ['oneortwotraonly_']:
+            autolabel2(h_pri, ax)
+
+    ax.legend(loc='upper left', fontsize='small')
 
     if ylim:
         ax.set_ylim(ylim)
@@ -339,11 +424,14 @@ def number_of_detections_vs_period_barchart(df, txt=None, ylim=None,
     ax.get_yaxis().set_tick_params(which='both', direction='in')
     ax.get_xaxis().set_tick_params(which='both', direction='in')
 
-    titlestr = os.path.basename(outdir).replace('_',' ')
-    ax.set_title(titlestr, fontsize='x-small')
+    if not forproposal:
+        titlestr = os.path.basename(outdir).replace('_',' ')
+        ax.set_title(titlestr, fontsize='x-small')
 
+    eoutstr='' if not forproposal else '_forproposal'
     savpath = os.path.join(
-        outdir,outstr+'number_of_detections_vs_period_barchart.png')
+        outdir,outstr+'number_of_detections_vs_plradius_barchart{}.png'.
+        format(eoutstr))
     fig.savefig(savpath, bbox_inches='tight', dpi=400)
     print('made {}'.format(savpath))
 
@@ -693,9 +781,9 @@ if __name__=="__main__":
         '../data/tommyb', 'detected_planet_catalog_{:s}.csv.bz2'.
         format(dn)) for dn in datanames]
 
-    one_thru_three = 1
-    four_thru_eight = 1
-    nine_thru_thirteen = 1
+    one_thru_three = 0
+    four_thru_eight = 0
+    nine_thru_thirteen = 0
     fourteen_thru_X =1
 
     for datapath, outdir in zip(datapaths, outdirs):
@@ -713,12 +801,17 @@ if __name__=="__main__":
                                                   ylim=(0.3,30))
             radius_insolation_diagram(df, outdir=outdir, xlim=(0.11, 2.5e5),
                                       ylim=(0.45,30))
+            radius_insolation_diagram(df, outdir=outdir, xlim=(0., 2.5),
+                                      ylim=(0,4), forproposal=True)
 
         if four_thru_eight:
 
             number_of_detections_vs_plradius_barchart(
                 df, txt='SNR>10 and $N_{\mathrm{tra,ext}} >= 3$',
                 ylim=(1,3800), outdir=outdir)
+            number_of_detections_vs_plradius_barchart(
+                df, txt='SNR>10 and $N_{\mathrm{tra,ext}} >= 3$',
+                ylim=(0.8,3), outdir=outdir, forproposal=True)
 
             Tmagcut=10
             number_of_detections_vs_plradius_barchart_Tmaglt(
@@ -730,7 +823,6 @@ if __name__=="__main__":
             number_of_detections_vs_ntra_barchart(
                 df, txt='SNR>10 and $N_{\mathrm{tra,ext}} >= 3$',
                 ylim=(0,1600), outdir=outdir)
-
 
             number_of_detections_vs_plradius_barchart(
                 df, txt='SNR>10 and $N_{\mathrm{tra,ext}} >= 3$ and just multis',
@@ -775,3 +867,7 @@ if __name__=="__main__":
             number_of_detections_vs_period_barchart(
                 df, txt='SNR>10 and $N_{\mathrm{tra,ext}} >= 3$',
                 ylim=(1,5000), outdir=outdir, yscale='log')
+
+            number_of_detections_vs_period_barchart(
+                df, txt='SNR>10 and $N_{\mathrm{tra,ext}} >= 3$',
+                ylim=(0,5), outdir=outdir, yscale='log', forproposal=True)
