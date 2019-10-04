f5f811c7f58a5a99a2e6de0e83e3a3fd3c8c2e9e
lgbouma <bouma.luke@gmail.com>
2019-10-04 11:58:40 -0400

    add idea 14 results

---
+++ results/copy_skymaps_real.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# PURPOSE: copy skymaps and give them nice names
+
+# cp visualize_survey_designs/primary_mission_eclmap_forproposal.png skymaps_real/primary_only_ecl.png
+# cp visualize_survey_designs/primary_mission_icrsmap_forproposal.png skymaps_real/primary_only_icrs.png
+# 
+# cp visualize_survey_designs/idea_14_SNE_eclmap_forproposal.png skymaps_real/extended_only_ecl.png
+# cp visualize_survey_designs/idea_14_SNE_icrsmap_forproposal.png skymaps_real/extended_only_icrs.png
+# 
+# cp visualize_survey_designs/merged_with_primary/idea_14_SNE_eclmap_forproposal.png skymaps_real/primary_plus_extended_ecl.png
+# cp visualize_survey_designs/merged_with_primary/idea_14_SNE_icrsmap_forproposal.png skymaps_real/primary_plus_extended_icrs.png
+
+# w/ k2 overplots
+
+cp visualize_survey_designs/primary_mission_eclmap_forproposal_forproposal_k2overplot.png skymaps_real/primary_only_ecl_k2kep.png
+cp visualize_survey_designs/primary_mission_icrsmap_forproposal_forproposal_k2overplot.png skymaps_real/primary_only_icrs_k2kep.png
+
+cp visualize_survey_designs/idea_14_SNE_eclmap_forproposal_forproposal_k2overplot.png skymaps_real/extended_only_ecl_k2kep.png
+cp visualize_survey_designs/idea_14_SNE_icrsmap_forproposal_forproposal_k2overplot.png skymaps_real/extended_only_icrs_k2kep.png
+
+cp visualize_survey_designs/merged_with_primary/idea_14_SNE_eclmap_forproposal_forproposal_k2overplot.png skymaps_real/primary_plus_extended_ecl_k2kep.png
+cp visualize_survey_designs/merged_with_primary/idea_14_SNE_icrsmap_forproposal_forproposal_k2overplot.png skymaps_real/primary_plus_extended_icrs_k2kep.png
+
